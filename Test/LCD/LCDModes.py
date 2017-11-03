22a4540b3f7a2521767439a5dfd2cae679dee1cf
Mattias Gustafsson <32227221+mattigustafsson@users.noreply.github.com>
2017-11-03 14:58:37 +0100

    La till fler under mappar

--- Test/LCD/LCDModes.py
+++ Test/LCD/LCDModes.py
@@ -6,4 +6,14 @@ lcd = LCD.Adafruit_CharLCDPlate()
 
 
 def standardMode():
-    "This is the standard mode for the LCD Screen. It displays the price and Status of the machine"
+    "This is the standard mode for the LCD screen. It displays the price and Status of the machine"
+    lcd.clear()
+    lcd.message("Pris:10kr\n")
+    lcd.message("Klar")
+
+def paymentMode():
+    "This is the state of the LCD screen when payment is being processed"
+    lcd.clear()
+    lcd.message("Payment is/nBeing processed")
+
+    
