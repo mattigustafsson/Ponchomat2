93312eb3ebf9d252fdc7956aaec3237b42484899
Mattias Gustafsson <32227221+mattigustafsson@users.noreply.github.com>
2017-11-03 15:41:54 +0100

    La till fler meddelade till skärmen

--- Test/LCD/LCDModes.py
+++ Test/LCD/LCDModes.py
@@ -1,19 +1,33 @@
-import time
+"""This is the diffirent messages for different senarios for the LCD screen"""
 
 import Adafruit_CharLCD as LCD
 
-lcd = LCD.Adafruit_CharLCDPlate()
 
 
-def standardMode():
-    "This is the standard mode for the LCD screen. It displays the price and Status of the machine"
-    lcd.clear()
-    lcd.message("Pris:10kr\n")
-    lcd.message("Klar")
+LCD = LCD.Adafruit_CharLCDPlate()
 
-def paymentMode():
-    "This is the state of the LCD screen when payment is being processed"
-    lcd.clear()
-    lcd.message("Payment is/nBeing processed")
 
-    
+def standard_mode():
+    """This is the standard mode for the LCD screen.
+    It displays the price and Status of the machine."""
+
+    LCD.clear()
+    LCD.message("Pris:10kr\n")
+    LCD.message("Klar")
+
+
+def payment_mode():
+    "This is the state of the LCD screen when payment is being processed."
+    LCD.clear()
+    LCD.message("Payment is/nBeing processed")
+
+
+def payment_insufficient():
+    "This state is when the payment unable to go through."
+    LCD.clear()
+    LCD.message("Card not approved")
+
+def display_balance(balance):
+    "Display current balance after purchase."
+    LCD.clear()
+    LCD.message(balance)
