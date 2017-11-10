6a04c27d8ed4dbe15abceaf465d7d2a9ba919739
Mattias Gustafsson <32227221+mattigustafsson@users.noreply.github.com>
2017-11-10 14:56:24 +0100

    Add button functions

--- Test/LCD/LCDModes.py
+++ Test/LCD/LCDModes.py
@@ -3,6 +3,12 @@
 import time
 import Adafruit_CharLCD as LCD
 
+# Char LCD plate button names.
+SELECT = 0
+RIGHT = 1
+DOWN = 2
+UP = 3
+LEFT = 4
 
 
 LCD = LCD.Adafruit_CharLCDPlate()
@@ -20,7 +26,7 @@ def standard_mode():
 def payment_mode():
     "This is the state of the LCD screen when payment is being processed."
     LCD.clear()
-    LCD.message("Payment is/n")
+    LCD.message("Payment is\n")
     LCD.message("Being processed")
 
 
@@ -33,7 +39,7 @@ def payment_insufficient():
 def display_balance(balance):
     "Display current balance after purchase."
     LCD.clear()
-    LCD.message("Card balance is:/n")
+    LCD.message("Card balance is:\n")
     LCD.message(balance)
 
 def main():
@@ -44,8 +50,21 @@ def main():
     time.sleep(3)
     payment_insufficient()
     time.sleep(3)
-    display_balance(400)
+    display_balance("400")
     time.sleep(3)
+
+    while True:
+        if LCD.is_pressed(SELECT):
+            standard_mode()
+        elif LCD.is_pressed(RIGHT):
+            payment_mode()
+        elif LCD.is_pressed(DOWN):
+            payment_insufficient()
+        elif LCD.is_pressed(UP):
+            display_balance("4564")
+        elif LCD.is_pressed(LEFT):
+            LCD.clear()
+            LCD.message("Adrian is\na bajsTolle")
 
 if __name__ == "__main__":
     main()
